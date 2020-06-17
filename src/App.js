c67fa6aa6aa59de510c67d558189a2b945aeca69
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-06-17 13:28:40 +0500

    clean up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import UserItem from "./components/users/UserItem";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
-
 class App extends React.Component {
     render() {
         return (
             <div className='App'>
-                <h1>hello from react</h1>
+                <Navbar />
+                <UserItem />
             </div>
         );
     }
