72f4a92a15b6d93689943ddb51237655b71c5d1f
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-06-20 20:15:08 +0500

    UI improvements
    Added loading spinner
    Added clear button

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,35 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-    state = {
-        id: "id",
-        login: "Arsalan Habib",
-        avatar_url: "https://avatars2.githubusercontent.com/u/54036702?v=4",
-        html_url: "https://github.com/Arsalan-Habib",
-    };
+const UserItem = ({ user: { login, html_url, avatar_url } }) => {
+    return (
+        <div className='card text-center'>
+            <img
+                src={avatar_url}
+                alt='profile'
+                className='round-img'
+                style={{ width: "60px" }}
+            />
 
-    render() {
-        const { login, html_url, avatar_url } = this.state;
+            <h3>{login}</h3>
 
-        return (
-            <div className='card text-center'>
-                <img
-                    src={avatar_url}
-                    alt='profile'
-                    className='round-img'
-                    style={{ width: "60px" }}
-                />
-
-                <h3>{login}</h3>
-
-                <div>
-                    <a href={html_url} className='btn btn-dark btn-sm my-1'>
-                        More Info
-                    </a>
-                </div>
+            <div>
+                <a href={html_url} className='btn btn-dark btn-sm my-1'>
+                    More Info
+                </a>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
