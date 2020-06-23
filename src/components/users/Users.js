3d1fc09cac0c543ceb2b5bd6f51c89f61f996366
Syed Arsalan Habib <arsalanhabib13@gmail.com>
2020-06-23 20:53:30 +0500

    Added responsiveness to the UI

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -12,7 +12,7 @@ const Users = () => {
         return <Spinner />;
     } else {
         return (
-            <div style={userStyle}>
+            <div className='grid-3'>
                 {users.map((user) => (
                     <UserItem key={user.id} user={user}></UserItem>
                 ))}
@@ -21,10 +21,11 @@ const Users = () => {
     }
 };
 
-const userStyle = {
-    display: "grid",
-    gridTemplateColumns: "repeat(3, 1fr)",
-    gridGap: "1rem",
-};
+// style={userStyle}
+// const userStyle = {
+//     display: "grid",
+//     gridTemplateColumns: "repeat(3, 1fr)",
+//     gridGap: "1rem",
+// };
 
 export default Users;
